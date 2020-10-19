6822df746a64b24ca57649ced7abef577f08bbe5
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-19 06:24:48 +0000

    Added the #choose_a_movie method to CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,7 +11,7 @@ class CLI
     sleep(1)
     puts "Welcome to the world of Studio Ghibli!"
     sleep(1)
-    puts "Would you like to see a list of films?"
+    puts "Would you like to see a list of our films?"
     sleep(1)
     puts "Type 'yes' to see the list; hit any other key to exit"
 
@@ -20,19 +20,40 @@ class CLI
     #if the user says yes
 
     if user_input == "yes" || user_input == "y"
-      puts "Excellent choice!"
+      puts "Excellent!  Here you are:"
       #display list of movies
       display_list_of_movies
+      choose_a_movie
     end
 
   end
 
   def display_list_of_movies
     sleep(1)
-    puts "ALL MOVIES:"
+    puts "ALL STUDIO GHIBLI MOVIES:"
     Movie.all.each.with_index(1) { |movie, index|
       puts "#{index}: #{movie.title}"
     }
+  end
+
+  def choose_a_movie
+      puts "Please enter the number of the movie you're interested in learning more about"
+      user_input = gets.strip.to_i
+      if user_input
+        puts "Great Choice!"
+        sleep(1)
+        puts " "
+        puts "Movie Title: " + Movie.all[user_input].title
+        puts "Directed by " + Movie.all[user_input].director
+        puts "Released in " + Movie.all[user_input].release_date
+        puts "Rotten Tomatoes Score:  " + Movie.all[user_input].rotten_tomatoes
+        puts "Synopsis: " + Movie.all[user_input].description
+      else
+        "Sorry, please enter a number"
+      end
+
+      puts "Would you like to choose a different movie?"
+
   end
 
 end
