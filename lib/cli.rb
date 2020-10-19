98591d2663903f9145eeb317b2fdc5133f0eaa45
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-19 06:35:44 +0000

    Refactored the CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,16 +3,22 @@ class CLI
   def start
     puts "Fetching Data..."
     API.fetch_movies
+    self.greeting
     self.main_menu
   end
 
-  def main_menu
-    #greet user, ask if they want to see a list of movies
+  def greeting
+    #greet user
     sleep(1)
     puts "Welcome to the world of Studio Ghibli!"
     sleep(1)
-    puts "Would you like to see a list of our films?"
+  end
+
+  def main_menu
+    #ask if they want to see a list of movies, main console
     sleep(1)
+    puts " "
+    puts "Would you like to see a list of our films?"
     puts "Type 'yes' to see the list; hit any other key to exit"
 
     user_input = gets.strip.downcase
@@ -22,8 +28,12 @@ class CLI
     if user_input == "yes" || user_input == "y"
       puts "Excellent!  Here you are:"
       #display list of movies
+      sleep(1)
       display_list_of_movies
       choose_a_movie
+    else
+      puts "Okay, come back soon!"
+      sleep(1)
     end
 
   end
@@ -48,11 +58,11 @@ class CLI
         puts "Released in " + Movie.all[user_input].release_date
         puts "Rotten Tomatoes Score:  " + Movie.all[user_input].rotten_tomatoes
         puts "Synopsis: " + Movie.all[user_input].description
+        sleep(3)
       else
-        "Sorry, please enter a number"
+        puts "Sorry, please enter a number"
       end
-
-      puts "Would you like to choose a different movie?"
+        main_menu
 
   end
 
